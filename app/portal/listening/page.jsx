529fa2e7bbce3f7a1e88ffa136465b353a69232b
Ahmed Gaafar <a.gaafar.junior@gmail.com>
2026-09-17 14:19:01 +0300

    Making standalone listening lessons more focused

--- app/portal/listening/page.jsx
+++ app/portal/listening/page.jsx
@@ -1,26 +1,20 @@
 // app/portal/listening/page.jsx
-import { fetchAllTranscriptLines, fetchVideos } from '../../../lib/queries/videos'
 import { fetchFreeLessonsByKind } from '../../../lib/queries/lessons'
 import { fetchStandaloneCompletedLessonKeys } from '../../../lib/queries/user'
 import ListeningClient from './ListeningClient'
 
 export default async function ListeningPage() {
-  const [lines, videos, lessons, completedKeys] = await Promise.all([
-    fetchAllTranscriptLines(),
-    fetchVideos(),
+  const [lessons, completedKeys] = await Promise.all([
     fetchFreeLessonsByKind('listening'),
     fetchStandaloneCompletedLessonKeys(),
   ])
 
-  const videosWithLines = videos.filter((v) => lines.some((l) => l.videoId === v.id))
   const completedIds = completedKeys
     .filter((k) => k.startsWith('lesson:'))
     .map((k) => k.slice('lesson:'.length))
 
   return (
     <ListeningClient
-      lines={lines}
-      videos={videosWithLines}
       lessons={lessons}
       completedIds={completedIds}
     />
